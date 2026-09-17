2889ddb1ccec0cd878dcd4bce62f982da8a73968
Abhishek  Patel (p46148) <146132979+saccharicap@users.noreply.github.com>
2026-09-17 17:00:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas
 
 # Load the saved model
-model = pandas.read_spss('linear_regression_model.sav', 'rb')
+model = joblib.load(open('linear_regression_model.sav', 'rb'))
 
 st.title('Sales Prediction App')
 
